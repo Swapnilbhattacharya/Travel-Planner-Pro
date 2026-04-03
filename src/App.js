c084fbcd8ea2f138437f603dac83028bd9c2d3b3
Swapnil Bhattacharya <swapnilmpbfhs@gmail.com>
2026-04-03 12:44:25 +0530

    System: Cleared boilerplate and integrated PlanProvider layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { PlanProvider } from './context/PlanContext';
+import Home from './pages/Home';
+import ItinerarySidebar from './components/ItinerarySidebar';
+// Note: We will create the Navbar next, or Chitresh can build it.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PlanProvider>
+      <div className="relative min-h-screen bg-gray-50 flex">
+        
+        {/* Main Content Area (Chitresh's Work) */}
+        <main className="flex-1 mr-80"> 
+          <header className="p-6 bg-white border-b sticky top-0 z-10">
+            <h1 className="text-2xl font-black tracking-tight text-blue-600">TRAVEL.PRO</h1>
+          </header>
+          
+          <Home />
+        </main>
+
+        {/* The Trip Plan Sidebar (Your Integration) */}
+        <aside>
+          <ItinerarySidebar />
+        </aside>
+
+      </div>
+    </PlanProvider>
   );
 }
 
